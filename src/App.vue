46d598dcf889a3f5cae6baaf65081e38859104d0
AlejandroPA76 <alejandrokeenegmail.com>
2023-08-02 01:05:58 -0600

    mi primer crud axios y vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,16 @@
+<!-- App.vue -->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <PostForm />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PostForm from './components/PostForm.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    PostForm,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,69 @@
+<!-- PostForm.vue -->
+<template>
+  <form @submit.prevent="submitForm">
+    <label for="name">Nombre:</label>
+    <input type="text" id="name" v-model="formData.nombres" required>
+
+    <label for="apellido">Apellido:</label>
+    <input type="text" id="apellido" v-model="formData.apellidos" required>
+
+    <button type="submit">Enviar</button>
+  </form>
+   <ul>
+      <!-- listar todos los datos del api-->
+      <li v-for="item in listClientes" :key="item.id">
+        {{item.nombres}} 
+        {{item.apellidos}} 
+        <button>show</button>{{}}
+        <button>edit</button>{{}}
+        <button>delete</button>
+    </li>
+    </ul>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      //recuerda sacar la lista del formData
+       listClientes:[],
+
+      formData: {
+      //las variables donde se guarde la informacion debe ser igual al de la api
+        nombres: '',
+        apellidos: '',
+      },
+    };
+  },
+   created() {
+    // Llamada GET utilizando Axios
+    axios.get('http://127.0.0.1:8000/api/clientes')
+      .then(response => {
+        // La respuesta de la API estará en la propiedad 'data' del objeto response
+        this.listClientes = response.data;
+      })
+      .catch(error => {
+        console.error('Error al obtener la lista de clientes:', error);
+        // Puedes manejar errores aquí
+      });
+  },
+
+  methods: {
+    submitForm() {
+      // Realizar la solicitud POST utilizando Axios
+      axios.post('http://127.0.0.1:8000/api/clientes', this.formData)
+        .then(response => {
+          console.log('Respuesta de la API:', response.data);
+          // Puedes manejar la respuesta de la API aquí
+        })
+        .catch(error => {
+          console.error('Error al hacer POST:', error);
+          // Puedes manejar errores aquí
+        });
+    },
+      
+  },
+};
+</script>
